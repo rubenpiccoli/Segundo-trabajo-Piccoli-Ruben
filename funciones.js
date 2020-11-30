ce0d063df3537e243c62a8874d7567ce54e4c299
rubenpiccoli <rubendariopiccoli@gmail.com>
2020-11-30 14:37:05 -0300

    tarea con Java terminada se probo y funciona correctamente se agrego efecto al boton jugar

--- funciones.js
+++ funciones.js
@@ -2,12 +2,25 @@ function Nombre() {
     //Consulta el nombre y la guarda en una varible Nombre
     var Ing_Nombre = prompt('¿Cual es tu Nombre?');
 
-    var Nombre = document.getElementById('nombre').innerHTML = "Hola " + Ing_Nombre;
+    if (Ing_Nombre == null || Ing_Nombre == "") {
+
+        alert('Ingrese su nombre para Jugar')
+
+    } else {
+
+        $('#exampleModal').modal('show');
+        var boton = document.getElementById('buuton');
+        boton.style.display = 'block';
+        document.getElementById('Ayuda').innerHTML = "";
+        document.getElementById('Ayuda1').innerHTML = "";
+        document.getElementById('ganaste').innerHTML = "";
+        contador = 0;
+        document.getElementById('nombre').innerHTML = "Hola " + Ing_Nombre;
+    }
 }
 
 
-
-var txtrespuesta = null;
+var txtrespuesta = "";
 var contador = 0;
 
 function Adivinanza() {
@@ -17,43 +30,66 @@ function Adivinanza() {
     if (Respuesta == "" || Respuesta == 0 || Respuesta > 6) {
 
         alert('Ingrese una respuesta del 1 al 6');
+        $('#txtrespuesta').val('');
         return
-
     }
     contador++
+
+
+
+    // verifica si la repuesta es correcta 
+    if (Respuesta == 1) {
+        var solucion = document.getElementById('ganaste').innerHTML = "Respuesta erronea!!! realizaste " + contador + " de 4 intentos";
+        $('#txtrespuesta').val('');
+
+    } else if (Respuesta == 2) {
+        var solucion = document.getElementById('ganaste').innerHTML = "Respuesta erronea !!! realizaste " + contador + " de 4 intentos";
+        $('#txtrespuesta').val('');
+
+    } else if (Respuesta == 3) {
+        var solucion = document.getElementById('ganaste').innerHTML = "Has ganado un descuento de 10%";
+        contador = 0
+        var boton = document.getElementById('buuton');
+        boton.style.display = 'none';
+        $('#txtrespuesta').val('');
+
+
+
+    } else if (Respuesta == 4) {
+        var solucion = document.getElementById('ganaste').innerHTML = "Respuesta erronea !!! realizaste " + contador + " de 4 intentos";
+        $('#txtrespuesta').val('');
+
+    } else if (Respuesta == 5) {
+        var solucion = document.getElementById('ganaste').innerHTML = "Respuesta erronea !!! realizaste " + contador + " de 4 intentos";
+        $('#txtrespuesta').val('');
+    } else if (Respuesta == 6) {
+        var solucion = document.getElementById('ganaste').innerHTML = "Respuesta erronea !!! realizaste " + contador + " de 4 intentos";
+        $('#txtrespuesta').val('');
+    }
 
     //Ayuda 2 intento
     if (contador == 2) {
 
         var solucion = document.getElementById('ganaste').innerHTML = "Respuesta erronea!!! realizaste " + contador + " de 4 intentos";
-        var Ayuda = document.getElementById('Ayuda').innerHTML = "Una de las sagas que realizo (Rambo)";
+        var Ayuda = document.getElementById('Ayuda').innerHTML = "1-Una de las sagas que realizo (Rambo)";
     }
     //ayuda 3 intento
     if (contador == 3) {
 
         var solucion = document.getElementById('ganaste').innerHTML = "Respuesta erronea!!! realizaste " + contador + " de 4 intentos";
-        var Ayuda = document.getElementById('Ayuda').innerHTML = "Otra de las sagas que realizo (Roqui)";
+        var Ayuda = document.getElementById('Ayuda1').innerHTML = "2-Otra de las sagas que realizo (Roqui)";
     }
     if (contador == 4) {
 
         var solucion = document.getElementById('ganaste').innerHTML = "Respuesta erronea!!! realizaste " + contador + " de 4 intentos";
-        alert('Has usado tus ' + contador + ' intentos perdiste!!!')
         var boton = document.getElementById('buuton');
         boton.style.display = 'none';
+        alert('Has usado tus ' + contador + ' intentos perdiste!!!')
     }
 
-    // verifica si la repuesta es correcta 
-    if (Respuesta == 1) {
-        var solucion = document.getElementById('ganaste').innerHTML = "Respuesta erronea!!! realizaste " + contador + " de 4 intentos";
-    } else if (Respuesta == 2) {
-        var solucion = document.getElementById('ganaste').innerHTML = "Respuesta erronea !!! realizaste " + contador + " de 4 intentos";
-    } else if (Respuesta == 3) {
-        var solucion = document.getElementById('ganaste').innerHTML = "Has ganado un descuento de 10%";
-        contador = 0
-    } else if (Respuesta == 4) {
-        var solucion = document.getElementById('ganaste').innerHTML = "Respuesta erronea !!! realizaste " + contador + " de 4 intentos";
-    }
+
 }
+
 //Solo permite introducir numeros.
 function soloNumeros(e) {
     var key = window.event ? e.which : e.keyCode;
